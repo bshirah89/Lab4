2fba83f4e2281e4820522b7f967900782dabad16
Brandon Shirah <shirbf@mail.broward.edu>
2017-03-29 18:50:32 -0400

    update
    got rid of use less variables

--- lab4.php
+++ lab4.php
@@ -3,14 +3,16 @@ require 'vendor/autoload.php';
 use Respect\Validation\Validator as validationObject;
 
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+$nameErr = $emailErr = "";
+$name = $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name = $_POST["name"];
 	$isValid = validationObject::stringType()->length(3,null)->validate($name);
   if( $isValid){
 		$nameErr = "Sorry Name must be more than 3 characters long";
+			}else{
+				$nameErr = "Correct!";
 			}
 	}
 
@@ -32,12 +34,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <p><span class="error"></span></p>
 <form method="post" action="lab4.php">  
   Name: <input type="text" name="name">
+  <span class="error"> <?php echo $nameErr ?></span>
   <br><br>
   E-mail: <input type="text" name="email">
   <span class="error">* </span>
-  <br><br>
-  Website: <input type="text" name="website">
-  <span class="error"></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
